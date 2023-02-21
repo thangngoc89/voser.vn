95275df59ae93108153d24ebf929fd0d8bb34d57
Khoa Nguyen <hi@khoanguyen.me>
2023-02-22 01:01:33 +0800

    release

---
+++ assets/home--loader-5e31be55.js
@@ -0,0 +1,1 @@
+import{i as o}from"./pocketbase-8cd5b0bf.js";import{j as r}from"./index-c66b48bc.js";const i=()=>r(o());export{i as loader};
---
+++ assets/home--loader-legacy-d4878160.js
@@ -0,0 +1,1 @@
+System.register(["./pocketbase-legacy-d584ed5d.js","./index-legacy-0393c597.js"],(function(e,t){"use strict";var s,r;return{setters:[e=>{s=e.i},e=>{r=e.j}],execute:function(){e("loader",(()=>r(s())))}}}));
