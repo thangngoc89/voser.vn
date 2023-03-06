4a6cf0ff0a83f833fcb55d218988ae2271356839
Khoa Nguyen <hi@khoanguyen.me>
2023-03-07 00:34:02 +0800

    release

---
+++ assets/home--loader-legacy-68f29502.js
@@ -0,0 +1,1 @@
+System.register(["./pocketbase-legacy-d584ed5d.js","./index-legacy-2e08a974.js"],(function(e,t){"use strict";var s,r;return{setters:[e=>{s=e.i},e=>{r=e.j}],execute:function(){e("loader",(()=>r(s())))}}}));
---
+++ assets/logout--loader-d4a73962.js
@@ -0,0 +1,1 @@
+import{Q as r,t as o,r as t}from"./index-1ad1b3d7.js";import{p as e}from"./pocketbase-8cd5b0bf.js";const n=async()=>(e.authStore.clear(),r.info(o`notify.logged out`),t("/"));export{n as loader};
