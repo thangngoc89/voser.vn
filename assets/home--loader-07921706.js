2cbd0c2afa019c111d6f56103a9939b3b85ed1c4
Khoa Nguyen <hi@khoanguyen.me>
2024-04-10 00:15:32 +0800

    release

---
+++ assets/home--loader-07921706.js
@@ -0,0 +1,1 @@
+import{i as o}from"./pocketbase-7569c031.js";import{j as r}from"./index-5ffe0fb5.js";const i=()=>r(o());export{i as loader};
---
+++ assets/types-e137fe7d.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-5ffe0fb5.js";function a(r){switch(r){case"mau_giao":return e`form.name.mau_giao`;case"tieu_hoc":return e`form.name.tieu_hoc`;case"nguoi_lon":return e`form.name.nguoi_lon`;case"who_children_surface":return e`form.name.who_chidlren_surface`;case"research_coidp":return e`form.name.research_coidp`}}export{a as s};
